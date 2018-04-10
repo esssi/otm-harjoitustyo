305e53cdd11c7d5a1c40e32524045c358a30d6d1
essi <essi.e.saario@gmail.com>
2018-04-10 16:24:04 +0300

    projektin 1. palautus

---
+++ Laskin/src/main/java/laskin/Calculator.java
@@ -0,0 +1,66 @@
+
+package laskin;
+
+public class Calculator {
+    private double currentValue;
+    
+    public Calculator(){
+        this.currentValue = 0;
+    }
+    
+    public void clear(){
+        currentValue = 0;
+    }
+    
+    public double getCurrentValue(){
+        return currentValue;
+    }
+    
+    public void add(double x){
+        currentValue += x;
+    }
+    
+    public void subtract(double x){
+        currentValue -= x;
+    }
+    
+    public void divideBy(double x){
+        if(x == 0){
+            return;
+        }
+        
+        currentValue /= x;
+    }
+    
+    public void multiplyBy(double x){
+        currentValue *= x;
+    }
+    
+    public void power(int n){
+        if(n < 0){
+            return;
+        }
+        
+        if(n == 0){
+            currentValue = 1;
+        }
+        
+        double base = currentValue;
+        for(int i = 1; i < n; i++){
+            multiplyBy(base);
+        }
+    }
+    
+    public void factorial(int n){
+        if(n == 0){
+            currentValue = 1;
+        } else {
+            for(int i = n - 1; i > 0; i--){
+                n *= i;
+            }
+            
+            currentValue = n;
+            // huom muuttaa laskimen arvon kertomaksi, ei lisää tms kertomaa
+        }   
+    }
+}
---
+++ Laskin/src/test/java/laskin/CalculatorTest.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laskin;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class CalculatorTest {
+    
+    Calculator c;
+ 
+    
+    @Before
+    public void setUp() {
+        c = new Calculator();
+    }
+    
+    @Test
+    public void currentValueIsZeroInTheBeginning(){
+        assertTrue(c.getCurrentValue() == 0);
+    }
+    
+    @Test
+    public void additionWorksRight(){
+        c.add(5);
+        
+        assertTrue(c.getCurrentValue() == 5);
+    }
+    
+    @Test
+    public void subtractionWorksRight(){
+        c.subtract(3);
+        
+        assertTrue(c.getCurrentValue() == -3);
+    }
+    
+    @Test
+    public void multiplicationWorksRight(){
+        c.add(2);
+        c.multiplyBy(3);
+        
+        assertTrue(c.getCurrentValue() == 6);
+    }
+    
+    @Test
+    public void divisionWorksForNonZeroDivider(){
+        c.add(8);
+        c.divideBy(4);
+        
+        assertTrue(c.getCurrentValue() == 2);
+    }
+    
+    @Test
+    public void dividingByZeroDoesNotChangeTheValue(){
+        c.add(1);
+        c.divideBy(0);
+        
+        assertTrue(c.getCurrentValue() == 1);
+    }
+    
+    @Test
+    public void exponentZeroGivesOne(){
+        c.add(10);
+        c.power(0);
+        
+        assertTrue(c.getCurrentValue() == 1);
+    }
+    
+    @Test
+    public void powerIsCorrect(){
+        c.add(2);
+        c.power(4);
+        
+        assertTrue(c.getCurrentValue() == 16);
+    }
+    
+    @Test
+    public void zeroFactorialEqualsOne(){
+        c.factorial(0);
+        
+        assertTrue(c.getCurrentValue() == 1);
+    }
+    
+    @Test
+    public void factorialGivesCorrectAnswer(){
+        c.factorial(9);
+        
+        assertTrue(c.getCurrentValue() == 362880);
+    }
+    
+}
